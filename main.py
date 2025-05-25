897f14a9a9e2b67322e197e0ea4f781730ad31a1
MoStar <42095935+Mo-101@users.noreply.github.com>
2025-05-25 08:22:31 +0000

    ok

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-from flask import Flask
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
-
-
-if __name__ == "__main__":
-    app.run()
+@app.get("/")
+def root():
+    return {"status": "Mastomy Natalensis DeepSight Awakens!"}
